a3a4365c806cf9193ebd6334a2e2aa0eea4249c2
Dan Hummon <dan.hummon@tartansolutions.com>
2011-08-08 18:45:04 -0400

    update to qx 1.5

--- application/designer2/generate.py
+++ application/designer2/generate.py
@@ -25,7 +25,7 @@
 import sys, os, re, subprocess
 
 CMD_PYTHON = sys.executable
-QOOXDOO_PATH = '../../../qooxdoo-1.4-sdk'
+QOOXDOO_PATH = '../../../qooxdoo-1.5-sdk'
 
 def getQxPath():
     path = QOOXDOO_PATH
--- framework/blueprint/generate.py
+++ framework/blueprint/generate.py
@@ -6,7 +6,7 @@
 #  http://qooxdoo.org
 #
 #  Copyright:
-#    2008 - 2009 1&1 Internet AG, Germany, http://www.1und1.de
+#    2008 - 2010 1&1 Internet AG, Germany, http://www.1und1.de
 #
 #  License:
 #    LGPL: http://www.gnu.org/licenses/lgpl.html
@@ -24,8 +24,8 @@
 
 import sys, os, re, subprocess
 
-CMD_PYTHON = 'python'
-QOOXDOO_PATH = '../../../qooxdoo-1.4-sdk'
+CMD_PYTHON = sys.executable
+QOOXDOO_PATH = '../../qooxdoo-1.5-sdk'
 
 def getQxPath():
     path = QOOXDOO_PATH
