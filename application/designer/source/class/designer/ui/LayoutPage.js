2560f18d1c82da17adda1ca609d1aa9bba2edc5d
Dan Hummon <dan.hummon@tartansolutions.com>
2011-08-17 17:25:31 -0400

    changes to layout page; adding a composite container

--- application/designer/source/class/designer/ui/LayoutPage.js
+++ application/designer/source/class/designer/ui/LayoutPage.js
@@ -4,9 +4,50 @@ qx.Class.define("designer.ui.LayoutPage",
 
   construct : function() {
     this.base(arguments, "Layout");
+    this.setPadding(2);
+    this.setLayout(new qx.ui.layout.Dock());
     
-    this.setLayout(new qx.ui.layout.Canvas());
+    var toolbar = new qx.ui.toolbar.ToolBar();
+    this.add(toolbar, {edge: "north"});
+    
+    var toolbarButton = new qx.ui.toolbar.Button("Add Something");
+    toolbar.add(toolbarButton);
+    
+    var outerContainer = new qx.ui.container.Composite(new qx.ui.layout.Grow());
+    this.add(outerContainer, {edge: "center"});
+    
+	var pane = new qx.ui.splitpane.Pane("horizontal");
+	
+	this._paneLeft = new qx.ui.container.Composite(new qx.ui.layout.Grow).set({
+		width : 200,
+		decorator : "main"
+	});
+	
+	this._paneRight = new qx.ui.container.Composite(new qx.ui.layout.Canvas()).set({
+		decorator : "main"
+	});
+
+	pane.add(this._paneLeft, 0);
+	pane.add(this._paneRight, 1);
+	outerContainer.add(pane);
+	
+	var thing = new designer.blueprint.ui.container.Composite({
+            "constructorSettings": {
+                "innerLayout": "qx.ui.layout.Canvas" 
+            },
+            "contents": [],
+            "objectClass": "blueprint.ui.container.Composite",
+            "objectId": "",
+            "qxSettings": {
+            	"backgroundColor": "red"
+            }
+    });
+    
+    this._paneRight.add(thing);
+  },
+  
+  members : {
+  	_paneLeft: null,
+  	_paneRight: null
   }
-  
-  
 });
