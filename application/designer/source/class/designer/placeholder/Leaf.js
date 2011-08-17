dc86afc9c3ab9a4814e423582ee1208e2d7f715c
Adams Tower <adams@ubuntu64-plaid-usedtobedev.tartansolutions.com>
2011-08-17 18:44:55 -0400

    leaf placeholder

---
+++ application/designer/source/class/designer/placeholder/Leaf.js
@@ -0,0 +1,84 @@
+/* ***********************************************
+
+Tartan Blueprint
+
+Copyright 2011 Tartan Solutions, Inc
+
+License:
+LGPL: http://www.gnu.org/licenses/lgpl.html
+EPL: http://www.eclipse.org/org/documents/epl-v10.php
+See the LICENSE file in the project's top-level directory for details.
+
+Authors:
+* Adams Tower
+*/
+
+
+/** TODOC
+ */
+qx.Class.define("designer.placeholder.Leaf", {
+    extend: qx.ui.core.Widget,
+
+    include: [
+        designer.util.MJson,
+        designer.util.MMovable,
+        designer.util.MResizable
+    ],
+
+    /** TODOC
+     */
+    construct: function(genId) {
+        this.setGeneratedId(genId);
+        this.setBackgroundColor("blue");
+        this.setRepClassName(qx.core.Init.getApplication().getManager().getObjectClass(this.getGeneratedId()));
+    },
+
+    properties: {
+        /** The class of the represented object. 
+         */
+        repClassName: {
+            check: "String"
+        }
+    },
+
+    members: {
+        /** @return Returns a list of all properties supported by the 
+         *  class of the represented object. 
+         */
+        getProperties: function() {
+            qx.core.Assert.assertNotUndefined(this.getRepClassName(), "This placeholder doesn't represent anything.");
+            return qx.Class.getProperties(qx.Class.getByName(this.getRepClassName()));
+        },
+        
+        /** @param name The name of the property to be defined.
+         *  @return Returns the definition of the given property.
+         */
+         getPropertyDefinition: function(name) {
+             qx.core.Assert.assertNotUndefined(this.getRepClassName(), "This placeholder doesn't represent anything.");
+             return qx.Class.getPropertyDefinition(qx.Class.getByName(this.getRepClassName()), name);
+         }
+         
+         /** DEPRECATED AT BIRTH
+          *  probably going to be replaced with just selectors
+          *  Sets a property by name.
+          *  @param prop The property to be set.
+          *  @param val The value the property will be set to.
+          */
+         setPropertyByName: function(prop, val) {
+             qx.core.Init.getApplication().getManager().setProperty(this.getGeneratedId, prop, val);
+         }
+         
+         /** DEPRECATED AT BIRTH
+          *  probably going to be replaced with just selectors
+          *  Gets a property by name.
+          *  @param prop The property to be set.
+          *  @returns Returns the value of the property.
+          */
+          getPropertyByName: function(prop) {
+              return qx.core.Init.getApplication().getManager().getProperty(this.getGeneratedId, prop);
+          }
+    },
+
+    destruct: function() {
+    }
+});
