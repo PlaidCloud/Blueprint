80725720bd66af2af9c82d416da4d735d3d6ac5f
Adams Tower <adams@ubuntu64-plaid-usedtobedev.tartansolutions.com>
2011-08-19 14:45:27 -0400

    extra ui elements

---
+++ application/designer/source/class/designer/blueprint/ui/basic/Image.js
@@ -0,0 +1,27 @@
+/* ***********************************************
+
+Tartan Blueprint
+
+Copyright 2011 Tartan Solutions, Inc
+
+License:
+LGPL: http://www.gnu.org/licenses/lgpl.html
+EPL: http://www.eclipse.org/org/documents/epl-v10.php
+See the LICENSE file in the project's top-level directory for details.
+
+Authors:
+* Adams Tower
+*/
+
+qx.Class.define("designer.blueprint.ui.basic.Image",
+{
+    extend  : blueprint.ui.basic.Image,
+    include : [
+        designer.util.MJson, 
+        designer.util.MDeafener,
+        designer.util.MMovable,
+        designer.util.MResizable,
+        designer.util.MReadOnly,
+        designer.util.MPropertyUtil
+    ]
+});
---
+++ application/designer/source/class/designer/blueprint/ui/tabview/TabView.js
@@ -0,0 +1,29 @@
+/* ***********************************************
+
+Tartan Blueprint
+
+Copyright 2011 Tartan Solutions, Inc
+
+License:
+LGPL: http://www.gnu.org/licenses/lgpl.html
+EPL: http://www.eclipse.org/org/documents/epl-v10.php
+See the LICENSE file in the project's top-level directory for details.
+
+Authors:
+* Adams Tower
+*/
+
+/**This probably needs a placeholder instead of this extension thing.
+ */
+qx.Class.define("designer.blueprint.ui.tabview.TabView",
+{
+    extend  : blueprint.ui.tabview.TabView,
+    include : [
+        designer.util.MJson, 
+        designer.util.MDeafener,
+        designer.util.MMovable,
+        designer.util.MHResizable,
+        designer.util.MReadOnly,
+        designer.util.MPropertyUtil
+    ]
+});
