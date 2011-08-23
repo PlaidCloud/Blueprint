a55f1835e90e7b42830c00822f03e16f0fe8571e
Dan Hummon <dan.hummon@tartansolutions.com>
2011-08-23 16:14:45 -0400

    Basic export functionality

--- application/designer/source/class/designer/core/manager/Blueprint.js
+++ application/designer/source/class/designer/core/manager/Blueprint.js
@@ -1,26 +1,34 @@
 qx.Class.define("designer.core.manager.Blueprint",
 {
-  extend : designer.core.manager.Abstract,
-  type : "singleton",
-
-  construct : function() {
-    this.base(arguments);
-  },
-
-  members :
-  {
-    /**
-     * TODOC
-     *
-     * @return {void} 
-     */
-    loadJson : function()
-    {
-      var request = new qx.io.remote.Request("resource/designer/examples/Login.json");
-
-      request.addListener("completed", this._processJson, this);
-
-      request.send();
-    }
-  }
+	extend : designer.core.manager.Abstract,
+	type : "singleton",
+	
+	construct : function() {
+		this.base(arguments);
+	},
+	
+	members :
+	{
+		/**
+		* TODOC
+		*
+		* @return {void} 
+		*/
+		loadJson : function()
+		{
+			var request = new qx.io.remote.Request("resource/designer/examples/Login.json");
+			
+			request.addListener("completed", this._processJson, this);
+			
+			request.send();
+		},
+		
+		exportJson : function() {
+			for (var i in this._objects) {
+				delete(this._objects[i]['__designer']);
+			}
+			
+			this.debug(qx.lang.Json.stringify(this._json, null, '\t'));
+		}
+	}
 });
---
+++ application/designer/source/class/designer/util/Misc.js
@@ -0,0 +1,28 @@
+/* ************************************************************************
+
+Tartan Blueprint
+
+http://www.tartansolutions.com
+
+Copyright:
+2008 - 2009 Tartan Solutions, Inc
+
+License:
+LGPL: http://www.gnu.org/licenses/lgpl.html
+EPL: http://www.eclipse.org/org/documents/epl-v10.php
+See the LICENSE file in the project's top-level directory for details.
+
+Authors:
+* Dan Hummon
+
+************************************************************************ */
+
+qx.Bootstrap.define("designer.util.Misc", {
+	type: "static",
+
+	statics: {
+		tbd : function() {
+			Throw new Error("Why did you call me?");
+		}
+	}
+});
