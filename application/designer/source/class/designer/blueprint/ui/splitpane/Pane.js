a92ab3380a0bc083147545d70c75926fe675c5dd
Adams Tower <adams@ubuntu64-plaid-usedtobedev.tartansolutions.com>
2011-08-31 14:24:23 -0400

    added some more designer.blueprint.ui objects. They are not very tested.

---
+++ application/designer/source/class/designer/blueprint/ui/splitpane/Pane.js
@@ -0,0 +1,26 @@
+/* ***********************************************
+
+Tartan Blueprint
+
+Copyright 2011 Tartan Solutions, Inc
+
+License:
+LGPL: http://www.gnu.org/licenses/lgpl.html
+EPL: http://www.eclipse.org/org/documents/epl-v10.php
+See the LICENSE file in the project's top-level directory for details.
+
+Authors:
+* Adams Tower
+*/
+
+qx.Class.define("designer.blueprint.ui.splitpane.Pane",
+{
+    extend  : blueprint.ui.splitpane.Pane,
+    include : [
+        designer.util.MJson
+        //designer.util.MDeafener,
+        //designer.util.MReadOnly,
+        //designer.util.MPropertyUtil,
+        //designer.util.MSelectable
+    ]
+});
