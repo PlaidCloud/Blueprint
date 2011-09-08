f858d07d2307d2f1fa980787fb1bf4403346330a
Adams Tower <adams@ubuntu64-plaid-usedtobedev.tartansolutions.com>
2011-09-08 17:15:38 -0400

    designer now can deal with menus, to a certain extent

--- application/designer/source/class/designer/blueprint/ui/toolbar/MenuButton.js
+++ application/designer/source/class/designer/blueprint/ui/toolbar/MenuButton.js
@@ -18,9 +18,18 @@ qx.Class.define("designer.blueprint.ui.toolbar.MenuButton",
     extend: designer.placeholder.EditableBranch,
     members: {
         editContents: function() {
-            this.debug("Adams, inside MenuButton's editContents");
+            //this.debug("Adams, inside MenuButton's editContents");
             var childrenIds = qx.core.Init.getApplication().getManager().getObjectContents(this.getGeneratedId());
-            this.debug("Adams, MenuButton, childrenIds: " + childrenIds);
+            //this.debug("Adams, MenuButton, childrenIds: " + childrenIds);
+            if (!this._editWindow) {
+                this._editWindow = new qx.ui.window.Window();
+                this._editWindow.setLayout(new qx.ui.layout.VBox());
+                for (var i=0; i<this._contents.length; i++) {
+                    //this.debug("Adams, MenuButton, adding " + this._contents[i]);
+                    this._editWindow.add(this._contents[i]);
+                }
+            }
+            this._editWindow.show();
         }
     }
 });
