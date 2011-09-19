d732731567f59a77c6bc1c3bead7206c4965ed5d
Adams Tower <adams@ubuntu64-plaid-usedtobedev.tartansolutions.com>
2011-09-19 19:26:36 -0400

    cosmetic changes to editable branches

--- application/designer/source/class/designer/blueprint/ui/toolbar/MenuButton.js
+++ application/designer/source/class/designer/blueprint/ui/toolbar/MenuButton.js
@@ -22,12 +22,16 @@ qx.Class.define("designer.blueprint.ui.toolbar.MenuButton",
             var childrenIds = qx.core.Init.getApplication().getManager().getObjectContents(this.getGeneratedId());
             //this.debug("Adams, MenuButton, childrenIds: " + childrenIds);
             if (!this._editWindow) {
-                this._editWindow = new qx.ui.window.Window();
+                this._editWindow = new qx.ui.window.Window(this.getRepClassName() + "(" + this.getGeneratedId() + ")");
                 this._editWindow.setLayout(new qx.ui.layout.VBox());
-                for (var i=0; i<this._contents.length; i++) {
-                    //this.debug("Adams, MenuButton, adding " + this._contents[i]);
-                    this._editWindow.add(this._contents[i]);
-                }
+                if (this._contents.length === 0) {
+                	this._editWindow.add(new qx.ui.basic.Label("No Contents."))
+                } else {
+		            for (var i=0; i<this._contents.length; i++) {
+		                //this.debug("Adams, MenuButton, adding " + this._contents[i]);
+		                this._editWindow.add(this._contents[i]);
+		            }
+		        }
             }
             this._editWindow.show();
         }
