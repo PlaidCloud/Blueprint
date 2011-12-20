ba3bf7e9a54d053a64e4b8eed3b88f4dd266eed8
Adams Tower <adams@plaid-ubuntu64-sinistar.hsd1.pa.comcast.net>
2011-12-20 15:22:40 -0500

    TreeView for menubutton

--- application/designer/source/class/designer/blueprint/ui/toolbar/MenuButton.js
+++ application/designer/source/class/designer/blueprint/ui/toolbar/MenuButton.js
@@ -18,22 +18,38 @@ qx.Class.define("designer.blueprint.ui.toolbar.MenuButton",
     extend: designer.placeholder.EditableBranch,
     members: {
         editContents: function() {
-            //this.debug("Adams, inside MenuButton's editContents");
-            var childrenIds = qx.core.Init.getApplication().getManager().getObjectContents(this.getGeneratedId());
-            //this.debug("Adams, MenuButton, childrenIds: " + childrenIds);
-            if (!this._editWindow) {
-                this._editWindow = new qx.ui.window.Window(this.getRepClassName() + "(" + this.getGeneratedId() + ")");
-                this._editWindow.setLayout(new qx.ui.layout.VBox());
-                if (this._contents.length === 0) {
-                	this._editWindow.add(new qx.ui.basic.Label("No Contents."))
-                } else {
-		            for (var i=0; i<this._contents.length; i++) {
-		                //this.debug("Adams, MenuButton, adding " + this._contents[i]);
-		                this._editWindow.add(this._contents[i]);
-		            }
-		        }
-            }
-            this._editWindow.show();
-        }
+			if (!this._editWindow) {
+				this._editWindow = new qx.ui.window.Window(this.getRepClassName() + "(" + this.getGeneratedId() + ")");
+				this._editWindow.setLayout(new qx.ui.layout.Dock());
+				var toolbar = new qx.ui.toolbar.ToolBar();
+				this._editWindow.add(toolbar, {edge:"north"});
+				toolbar.add(new qx.ui.toolbar.Button("I do nothing"));
+				var hierarchy = this.getHierarchy();
+				
+				var nodes = qx.data.marshal.Json.createModel(hierarchy, true);
+
+				var tree = new qx.ui.tree.VirtualTree(nodes, "name", "children");
+				this._editWindow.add(tree, {edge:"center"});
+			}
+
+			this._editWindow.show();
+        },
+
+		getHierarchy: function(objId) {
+			if (!objId) {
+				objId = this.getGeneratedId();
+			}
+			var name = objId;
+			var contents = qx.core.Init.getApplication().getManager().getObjectContents(objId);
+			if (contents.length == 0) {
+				return {"name": name};
+			} else {
+				var children = [];
+				for (var i=0; i<contents.length; i++) {
+					children.push(this.getHierarchy(contents[i]));
+				}
+				return {"name": name, "children": children};
+			}
+		}
     }
 });
