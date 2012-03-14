01babac18b19984e86fab57ff00fbdd1794ac8e6
Dan Hummon <dan.hummon@tartansolutions.com>
2012-03-14 14:42:22 -0400

    blueprint.ui.window.Desktop

---
+++ framework/blueprint/source/class/blueprint/ui/window/Desktop.js
@@ -0,0 +1,36 @@
+/* ************************************************************************
+
+Tartan Blueprint
+
+http://www.tartansolutions.com
+
+Copyright:
+2008 - 2009 Tartan Solutions, Inc
+
+License:
+LGPL: http://www.gnu.org/licenses/lgpl.html
+EPL: http://www.eclipse.org/org/documents/epl-v10.php
+See the LICENSE file in the project's top-level directory for details.
+
+Authors:
+* Dan Hummon
+
+************************************************************************ */
+
+qx.Class.define("blueprint.ui.window.Desktop", {
+	extend: qx.ui.window.Desktop,
+
+	include: [
+	blueprint.MBlueprintManager
+	],
+
+	/**
+	* @param vData {Object}
+	*	The JSON object describing this widget.
+	*/
+	construct: function(vData, namespace, skipRecursion) {
+		this.base(arguments);
+
+		this.set(vData.qxSettings);
+	}
+});
